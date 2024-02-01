09e36f2d71542f9a048bc52746ec252e66fc66da
iqrarasool04 <iqrarasool45@gmail.com>
2024-02-01 18:51:06 +0500

    time cal

--- lab1.py
+++ lab1.py
@@ -2,15 +2,16 @@ import time
 
 num_Steps = 1000
 steps = 1/num_Steps
+sum = 0.0
 
-start = time.time()
-for i in num_Steps:
+start = time.perf_counter()
+for i in range(num_Steps):
     x = (i + 0.5) * steps
     sum = sum + 4.0 / (1 + x*x)
 
 pi = steps * sum
 print("PI ",pi)
-end = time.time()
+end = time.perf_counter()
 
-execution_time = end - start
-print("PI calculation took ", execution_time)
+execution_time = (end - start) * 1e6
+print("PI calculation took ", execution_time, " microsec to run.")
